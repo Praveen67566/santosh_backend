69fa790c24b7c5263ba350926b829fd31614639a
praveen67566 <dwivedipraveendhar@gmail.com>
2025-04-12 15:21:44 +0530

    getcurrentuser

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -190,3 +190,18 @@ export const adminlogin = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+export const getCurrentUser = async (req, res) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -1,7 +1,8 @@
 import express from "express"
-import { register,login } from "../Controllers/userController.js";
+import { register,login, getCurrentUser } from "../Controllers/userController.js";
 
 export const userRouter = express.Router();
 
 userRouter.post('/register',register);
 userRouter.post('/login',login);
+userRouter.get('/user',getCurrentUser);
