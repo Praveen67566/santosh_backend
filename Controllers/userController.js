d8e190d59c94b598f07881437ae45f00780af87d
AniketVerma8808 <aniketverma000081@gmail.com>
2025-04-12 15:41:39 +0530

    aded

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -190,11 +190,16 @@ export const adminlogin = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
 export const getCurrentUser = async (req, res) => {
   try {
-    const token = req.cookies.token;
-    if (!token) return res.status(401).json({ message: "Unauthorized" });
+    const authHeader = req.headers.authorization;
 
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.id).select("-password");
 
