defb756b59a5110a778f1010586f3111de76e7c0
praveen67566 <dwivedipraveendhar@gmail.com>
2025-04-19 15:41:39 +0530

    payment and active membership added

--- Controllers/membershipController.js
+++ Controllers/membershipController.js
@@ -1,2 +1,43 @@
 import { Membership } from "../Models/membershipModel.js";
+import { Payment } from "../Models/PaymentModel.js";
 
+export const activatemembership = async (req,res)=>{
+    try {
+       const id = req.params;
+       const {status} = req.body;
+
+       if(!id || !status){
+        res.status(400).json({message:"id and status are required"})
+       }
+       
+       const updatedMembership = await Membership.findByIdAndUpdate(
+        id, // just the ID, no object wrapper
+        { status: "Active" },
+        { new: true } // returns the updated document
+      );
+
+      if(!updatedMembership){
+        res.status(400).json({message:"Unable to update membership"})
+      }
+      
+      const paymentid = updatedMembership.payment;
+
+      if(!paymentid){
+        res.status(400).json({message:"payment not done yet"});
+      }
+      
+
+      const pay = Payment.findByIdAndUpdate(paymentid,
+        {status:'Paid'},
+        {new:true}
+      )
+
+      if(!pay){
+        res.status(400).json({message:"unable to change status"});
+      }
+
+      res.status(200).json({message:"Membership activated successfully",pay,updatedMembership});
+    } catch (error) {
+        res.status(500).json({message:"Internal Server Error"});
+    }
+}
--- Routers/membershipRouter.js
+++ Routers/membershipRouter.js
@@ -1,4 +1,7 @@
 import express from "express";
+import { activatemembership } from "../Controllers/membershipController.js";
 // import { makePaymentFormembership } from "../Controllers/membershipController.js";
 
 export const membershipRouter = express.Router();
+
+membershipRouter.put('/activatemembership',activatemembership);
