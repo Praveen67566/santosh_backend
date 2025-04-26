5b8104448d6e5fe80671b7eca6ba2374470b0254
praveen67566 <dwivedipraveendhar@gmail.com>
2025-04-26 15:23:47 +0530

    just

--- Middleware/authmiddleware.js
+++ Middleware/authmiddleware.js
@@ -1,14 +1,15 @@
 import jwt from "jsonwebtoken";
 export const protect = async (req, res, next) => {
   try {
-    const token = req.cookies.token || req.headers.authorization.split(" ")[1];
+    // Check if the token exists either in cookies or in the Authorization header
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
 
     // Check if token exists
     if (!token) {
       return res.status(401).json({ message: "Unauthorized" });
     }
 
-    // Verify token
+    // Verify the token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     // Optionally attach user info to request (if needed later)
@@ -20,3 +21,4 @@ export const protect = async (req, res, next) => {
     return res.status(401).json({ message: "Invalid or expired token" });
   }
 };
+
