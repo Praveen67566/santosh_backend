e459c7112680cbe5bb4d82219dca4b69a66d7663
praveen67566 <dwivedipraveendhar@gmail.com>
2025-04-26 15:20:42 +0530

    just

--- Middleware/authmiddleware.js
+++ Middleware/authmiddleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 export const protect = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    const token = req.cookies.token || req.headers.authorization.split(" ")[1];
 
     // Check if token exists
     if (!token) {
