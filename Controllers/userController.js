3d120c03e859823b73597510937adb871996f0c8
praveen67566 <dwivedipraveendhar@gmail.com>
2025-04-26 15:26:23 +0530

    just

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -241,15 +241,7 @@ export const getCurrentUser = async (req, res) => {
 
 export const getReferralLink = async (req, res) => {
   try {
-    const token = req.cookies.token;
-
-    if (!token) {
-      return res.status(401).json({ message: "Unauthorized" });
-    }
-
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.id).select("-password");
+    const user = req.user;
 
     if (!user) return res.status(404).json({ message: "User not found" });
 
