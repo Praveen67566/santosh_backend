72e8c781a9d2060aadaf6951004354c1225f2589
praveen67566 <dwivedipraveendhar@gmail.com>
2025-04-29 11:25:59 +0530

    getallwithdrawals

--- Controllers/withdrawalController.js
+++ Controllers/withdrawalController.js
@@ -1,4 +1,5 @@
 import { Withdrawal } from "../Models/withdrawModel.js";
+import { User } from "../Models/userModel.js";
 
 export const requestwithdrawal = async (req,res)=>{
      try {
@@ -24,4 +25,25 @@ export const requestwithdrawal = async (req,res)=>{
      } catch (error) {
         res.status(500).json({message:"Internal Server Error"})
      }
+}
+
+export const getallwithdrawal = async (req,res)=>{
+   try {
+      const withdrawal = await Withdrawal.find({});
+      let users = [];
+
+      for (let i = 0; i < withdrawal.length; i++) {
+        const userid = withdrawal[i].userid;
+
+        const users = await User.findOne({userid});
+         
+      }
+      res.status(201).json({withdrawal,users});
+
+      if(!users){
+         res.status(404).json({message:"No user found"});
+      }
+   } catch (error) {
+     res.status(500).json({message:"Internal Server Error"});
+   }
 }
