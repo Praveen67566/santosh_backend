cc554c42ad509b802b7e1e07a45991981adb7713
AniketVerma8808 <aniketverma000081@gmail.com>
2025-04-29 12:55:47 +0530

    adeD

--- Controllers/withdrawalController.js
+++ Controllers/withdrawalController.js
@@ -1,54 +1,53 @@
 import { Withdrawal } from "../Models/withdrawModel.js";
 import { User } from "../Models/userModel.js";
 
-export const requestwithdrawal = async (req,res)=>{
-     try {
-        const {userid,amount,accountNo,ifsccode,upiId} = req.body;
+export const requestwithdrawal = async (req, res) => {
+  try {
+    const { userid, amount, accountNo, ifsccode, upiId } = req.body;
 
-        if(!userid){
-         res.status(400).json({message:"UserId is required"});
-        }
+    if (!userid) {
+      res.status(400).json({ message: "UserId is required" });
+    }
 
-        const user = await User.findOne({_id:userid});
+    const user = await User.findOne({ _id: userid });
 
-        if(!user){
-         
-        }
-        const withdrawal = await Withdrawal.create({
-            userid,
-            amount,
-            accountNo,
-            ifsccode,
-            upiId
-        });
+    if (!user) {
+    }
+    const withdrawal = await Withdrawal.create({
+      userid,
+      amount,
+      accountNo,
+      ifsccode,
+      upiId,
+    });
 
-        if(!withdrawal){
-           res.status(500).json({message:"Server Error"})
-        }
+    if (!withdrawal) {
+      res.status(500).json({ message: "Server Error" });
+    }
 
-        res.status(201).json({withdrawal});
-     } catch (error) {
-        res.status(500).json({message:"Internal Server Error"})
-     }
-}
+    res.status(201).json({ withdrawal });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-export const getallwithdrawal = async (req,res)=>{
-   try {
-      const withdrawal = await Withdrawal.find({});
-      let users = [];
+export const getallwithdrawal = async (req, res) => {
+  try {
+    const withdrawal = await Withdrawal.find({});
+    let users = [];
 
-      for (let i = 0; i < withdrawal.length; i++) {
-        const userid = withdrawal[i].userid;
+    for (let i = 0; i < withdrawal.length; i++) {
+      const userid = withdrawal[i].userid;
+      console.log(userid);
+      const user = await User.findOne({ _id: userid });
+      users.push(user);
+    }
+    res.status(201).json({ withdrawal, users });
 
-        const users = await User.findOne({userid});
-         
-      }
-      res.status(201).json({withdrawal,users});
-
-      if(!users){
-         res.status(404).json({message:"No user found"});
-      }
-   } catch (error) {
-     res.status(500).json({message:"Internal Server Error"});
-   }
-}
+    if (!users) {
+      res.status(404).json({ message: "No user found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
